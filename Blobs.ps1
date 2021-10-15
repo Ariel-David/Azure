009437d0ee6614ec148a1d947aac0bc744f88dd0
Ariel David <57994211+Ariel-David@users.noreply.github.com>
2021-10-15 17:24:38 +0300

    Update Blobs.ps1

--- Blobs.ps1
+++ Blobs.ps1
@@ -1,16 +1,27 @@
-$ResourceGroupName= "myResourceGroup"
+$ResourceGroupName= "ArielResourceGroup"
 $AStorageAccountName = "arielstoragesccount1"
 $BStorageAccountName = "arielstorageaccount2"
 $AStorageAccountContext = (Get-AzStorageAccount -ResourceGroupName $ResourceGroupName -Name $AStorageAccountName).Context
 $BStorageAccountContext = (Get-AzStorageAccount -ResourceGroupName $ResourceGroupName -Name $BStorageAccountName).Context
+
 $AcontainerName = "acontainer"
 $BcontainerName = "bcontainer"
+
+$AContext = New-AzStorageContext -StorageAccountName $AStorageAccountName
+$BContext = New-AzStorageContext -StorageAccountName $BStorageAccountName
+
+New-AzStorageContainer -Name $AcontainerName -Context $AContext -Permission blob
+New-AzStorageContainer -Name $BcontainerName -Context $BContext -Permission blob
+
 $AStorageKey = Get-AzStorageAccountKey -Name $AStorageAccountName `
 -ResourceGroupName $ResourceGroupName 
+
 $AContext = New-AzStorageContext -StorageAccountName $AStorageAccountName `
 -StorageAccountKey $AStorageKey.Value[0] 
-Get-ChildItem -Path C:\Users\ariel\Microsoft Azure\Blobs | Set-AzStorageBlobContent -Container $AContainer `
+
+Get-ChildItem -Path C:\Users\ariel\Microsoft.Azure\Blobs | Set-AzStorageBlobContent -Container $AcontainerName `
 -Context $AContext -Force
+
 azcopy copy "https://arielstoragesccount1.blob.core.windows.net/?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacuptfx&se=2022-10-15T20:51:53Z&st=2021-10-15T12:51:53Z&spr=https&sig=MFe3La0BLrmjqE7DZaBoJnyh0eo5nnqpv1EeTpHzk7Q%3D" "https://arielstorageaccount2.blob.core.windows.net/?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacuptfx&se=2022-10-15T20:52:39Z&st=2021-10-15T12:52:39Z&spr=https&sig=lKd5139LYE%2FTN7q3vxXUF3x0jLKbOgZNDgA4UIRZa9w%3D" --recursive=true
 
 
